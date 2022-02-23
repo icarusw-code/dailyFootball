610bb14e73f503bafc1277bb7e4b63698b9571f0
limgeonho <ghlim909@gmail.com>
2022-02-24 00:00:36 +0900

    daily football start

---
+++ backend/src/main/java/DailyFootball/demo/DailyFootballApplication.java
@@ -0,0 +1,13 @@
+package DailyFootball.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyFootballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyFootballApplication.class, args);
+	}
+
+}
---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Title = styled.h2``;
+
+function App() {
+  return (
+    <div>
+      <Title>Daily Football</Title>
+    </div>
+  );
+}
+
+export default App;
